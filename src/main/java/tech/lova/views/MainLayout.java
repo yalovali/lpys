e1a7acdb6b2ad152bf6c8f26eb76135016d19276
Yasin Yılmaz <yalovali@gmail.com>
2025-07-11 20:41:21 +0300

    added side navigation

--- src/main/java/tech/lova/views/MainLayout.java
+++ src/main/java/tech/lova/views/MainLayout.java
@@ -1,5 +1,8 @@
 package tech.lova.views;
 
+import java.io.ByteArrayInputStream;
+import java.util.Optional;
+
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.avatar.Avatar;
@@ -11,7 +14,6 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Header;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.icon.SvgIcon;
 import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.sidenav.SideNav;
@@ -23,9 +25,8 @@ import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.flow.server.menu.MenuConfiguration;
 import com.vaadin.flow.server.menu.MenuEntry;
 import com.vaadin.flow.theme.lumo.LumoUtility;
-import java.io.ByteArrayInputStream;
-import java.util.List;
-import java.util.Optional;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+
 import tech.lova.data.User;
 import tech.lova.security.AuthenticatedUser;
 
@@ -36,99 +37,138 @@ import tech.lova.security.AuthenticatedUser;
 @AnonymousAllowed
 public class MainLayout extends AppLayout {
 
-    private H1 viewTitle;
+	private H1 viewTitle;
 
-    private AuthenticatedUser authenticatedUser;
-    private AccessAnnotationChecker accessChecker;
+	private AuthenticatedUser authenticatedUser;
+	private AccessAnnotationChecker accessChecker;
 
-    public MainLayout(AuthenticatedUser authenticatedUser, AccessAnnotationChecker accessChecker) {
-        this.authenticatedUser = authenticatedUser;
-        this.accessChecker = accessChecker;
+	public MainLayout(AuthenticatedUser authenticatedUser, AccessAnnotationChecker accessChecker) {
+		this.authenticatedUser = authenticatedUser;
+		this.accessChecker = accessChecker;
 
-        setPrimarySection(Section.DRAWER);
-        addDrawerContent();
-        addHeaderContent();
-    }
+		setPrimarySection(Section.DRAWER);
+		addDrawerContent();
+		addHeaderContent();
+	}
 
-    private void addHeaderContent() {
-        DrawerToggle toggle = new DrawerToggle();
-        toggle.setAriaLabel("Menu toggle");
+	private void addDrawerContent() {
+		Span appName = new Span("LPYS");
+		appName.addClassNames(LumoUtility.FontWeight.SEMIBOLD, LumoUtility.FontSize.LARGE);
+		Header header = new Header(appName);
 
-        viewTitle = new H1();
-        viewTitle.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
+		Scroller scroller = new Scroller(createSideNav());
 
-        addToNavbar(true, toggle, viewTitle);
-    }
+		addToDrawer(header, scroller, createFooter());
+	}
 
-    private void addDrawerContent() {
-        Span appName = new Span("LPYS");
-        appName.addClassNames(LumoUtility.FontWeight.SEMIBOLD, LumoUtility.FontSize.LARGE);
-        Header header = new Header(appName);
+	private void addHeaderContent() {
+		DrawerToggle toggle = new DrawerToggle();
+		toggle.setAriaLabel("Menu toggle");
 
-        Scroller scroller = new Scroller(createNavigation());
+		viewTitle = new H1();
+		viewTitle.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
 
-        addToDrawer(header, scroller, createFooter());
-    }
+		addToNavbar(true, toggle, viewTitle);
+	}
 
-    private SideNav createNavigation() {
-        SideNav nav = new SideNav();
+	@Override
+	protected void afterNavigation() {
+		super.afterNavigation();
+		viewTitle.setText(getCurrentPageTitle());
+	}
 
-        List<MenuEntry> menuEntries = MenuConfiguration.getMenuEntries();
-        menuEntries.forEach(entry -> {
-            if (entry.icon() != null) {
-                nav.addItem(new SideNavItem(entry.title(), entry.path(), new SvgIcon(entry.icon())));
-            } else {
-                nav.addItem(new SideNavItem(entry.title(), entry.path()));
-            }
-        });
+	private Footer createFooter() {
+		Footer layout = new Footer();
 
-        return nav;
-    }
+		Optional<User> maybeUser = authenticatedUser.get();
+		if (maybeUser.isPresent()) {
+			User user = maybeUser.get();
 
-    private Footer createFooter() {
-        Footer layout = new Footer();
+			Avatar avatar = new Avatar(user.getName());
+			StreamResource resource = new StreamResource("profile-pic",
+					() -> new ByteArrayInputStream(user.getProfilePicture()));
+			avatar.setImageResource(resource);
+			avatar.setThemeName("xsmall");
+			avatar.getElement().setAttribute("tabindex", "-1");
 
-        Optional<User> maybeUser = authenticatedUser.get();
-        if (maybeUser.isPresent()) {
-            User user = maybeUser.get();
+			MenuBar userMenu = new MenuBar();
+			userMenu.setThemeName("tertiary-inline contrast");
 
-            Avatar avatar = new Avatar(user.getName());
-            StreamResource resource = new StreamResource("profile-pic",
-                    () -> new ByteArrayInputStream(user.getProfilePicture()));
-            avatar.setImageResource(resource);
-            avatar.setThemeName("xsmall");
-            avatar.getElement().setAttribute("tabindex", "-1");
+			MenuItem userName = userMenu.addItem("");
+			Div div = new Div();
+			div.add(avatar);
+			div.add(user.getName());
+			div.add(new Icon("lumo", "dropdown"));
+			div.addClassNames(LumoUtility.Display.FLEX, LumoUtility.AlignItems.CENTER, LumoUtility.Gap.SMALL);
+			userName.add(div);
+			userName.getSubMenu().addItem("Sign out", e -> {
+				authenticatedUser.logout();
+			});
 
-            MenuBar userMenu = new MenuBar();
-            userMenu.setThemeName("tertiary-inline contrast");
+			layout.add(userMenu);
+		}
+		else {
+			Anchor loginLink = new Anchor("login", "Sign in");
+			layout.add(loginLink);
+		}
 
-            MenuItem userName = userMenu.addItem("");
-            Div div = new Div();
-            div.add(avatar);
-            div.add(user.getName());
-            div.add(new Icon("lumo", "dropdown"));
-            div.addClassNames(LumoUtility.Display.FLEX, LumoUtility.AlignItems.CENTER, LumoUtility.Gap.SMALL);
-            userName.add(div);
-            userName.getSubMenu().addItem("Sign out", e -> {
-                authenticatedUser.logout();
-            });
+		return layout;
+	}
 
-            layout.add(userMenu);
-        } else {
-            Anchor loginLink = new Anchor("login", "Sign in");
-            layout.add(loginLink);
-        }
+	/**
+	 * Creates the side navigation menu.
+	 *
+	 * The navigation menu is dynamically populated with menu entries from
+	 * `MenuConfiguration`. Each entry is represented as a `SideNavItem` with
+	 * optional icons.
+	 *
+	 * @return A `SideNav` component containing the navigation items.
+	 */
+	private SideNav createSideNav() {
+		var nav = new SideNav(); // Create the side navigation
+		nav.addClassNames(Margin.Horizontal.MEDIUM); // Style the navigation
+		MenuConfiguration.getMenuEntries().forEach(entry -> createSideNavItem(nav, entry)); // Add menu
+																							// entries
+		return nav;
+	}
 
-        return layout;
-    }
+	/**
+	 * Creates a side navigation item for a given menu entry.
+	 *
+	 * Each menu entry is represented as a `SideNavItem` with optional icons.
+	 *
+	 * @param menuEntry The menu entry to create a navigation item for.
+	 * @return A `SideNavItem` representing the menu entry.
+	 */
+	private void createSideNavItem(SideNav nav, MenuEntry menuEntry) {
+		if (menuEntry == null)
+			return; // Return null if the menu entry is null
+		// read the menu entry properties
+		String title = menuEntry.title();
+		String path = menuEntry.path();
+		String icon = menuEntry.icon();
+		// if title contains a dot, it is a sub-menu entry
+		if (title.contains(".")) {
+			var parts = title.split("\\.");
+			title = parts[parts.length - 1]; // Use the last part as the title
+			String parent_title = parts[0]; // Use the first part as the parent title
+			// find the parent menu entry
+			SideNavItem parentItem = nav.getItems().stream().filter(item -> item.getLabel().equals(parent_title))
+					.findFirst().orElse(null);
+			if (parentItem == null) {
+				parentItem = new SideNavItem(parent_title);
+				parentItem.setPrefixComponent(new Icon(icon)); // Set the icon for the parent item
+				nav.addItem(parentItem); // Add the parent item to the navigation
+			}
+			// Create a sub-menu item under the parent entry
+			parentItem.addItem(new SideNavItem(title, path, new Icon(icon)));
+		}
+		else
+			// Create a top-level menu item
+			nav.addItem(new SideNavItem(title, path, new Icon(icon))); // Create item with
+	}
 
-    @Override
-    protected void afterNavigation() {
-        super.afterNavigation();
-        viewTitle.setText(getCurrentPageTitle());
-    }
-
-    private String getCurrentPageTitle() {
-        return MenuConfiguration.getPageHeader(getContent()).orElse("");
-    }
+	private String getCurrentPageTitle() {
+		return MenuConfiguration.getPageHeader(getContent()).orElse("");
+	}
 }
--- src/main/java/tech/lova/views/masterdetail/MasterDetailView.java
+++ src/main/java/tech/lova/views/masterdetail/MasterDetailView.java
@@ -1,5 +1,9 @@
 package tech.lova.views.masterdetail;
 
+import java.util.Optional;
+
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -27,190 +31,187 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
-import java.util.Optional;
-import org.springframework.orm.ObjectOptimisticLockingFailureException;
-import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
 import tech.lova.data.SamplePerson;
 import tech.lova.services.SamplePersonService;
+//@Menu(order = 1, icon = LineAwesomeIconUrl.COLUMNS_SOLID)
 
 @PageTitle("Master-Detail")
 @Route("master-detail/:samplePersonID?/:action?(edit)")
-@Menu(order = 1, icon = LineAwesomeIconUrl.COLUMNS_SOLID)
+@Menu(order = 0, icon = "vaadin:users", title = "Settings.Users")
 @AnonymousAllowed
 @Uses(Icon.class)
 public class MasterDetailView extends Div implements BeforeEnterObserver {
 
-    private final String SAMPLEPERSON_ID = "samplePersonID";
-    private final String SAMPLEPERSON_EDIT_ROUTE_TEMPLATE = "master-detail/%s/edit";
+	private final String SAMPLEPERSON_ID = "samplePersonID";
+	private final String SAMPLEPERSON_EDIT_ROUTE_TEMPLATE = "master-detail/%s/edit";
 
-    private final Grid<SamplePerson> grid = new Grid<>(SamplePerson.class, false);
+	private final Grid<SamplePerson> grid = new Grid<>(SamplePerson.class, false);
 
-    private TextField firstName;
-    private TextField lastName;
-    private TextField email;
-    private TextField phone;
-    private DatePicker dateOfBirth;
-    private TextField occupation;
-    private TextField role;
-    private Checkbox important;
+	private TextField firstName;
+	private TextField lastName;
+	private TextField email;
+	private TextField phone;
+	private DatePicker dateOfBirth;
+	private TextField occupation;
+	private TextField role;
+	private Checkbox important;
 
-    private final Button cancel = new Button("Cancel");
-    private final Button save = new Button("Save");
+	private final Button cancel = new Button("Cancel");
+	private final Button save = new Button("Save");
 
-    private final BeanValidationBinder<SamplePerson> binder;
+	private final BeanValidationBinder<SamplePerson> binder;
 
-    private SamplePerson samplePerson;
+	private SamplePerson samplePerson;
 
-    private final SamplePersonService samplePersonService;
+	private final SamplePersonService samplePersonService;
 
-    public MasterDetailView(SamplePersonService samplePersonService) {
-        this.samplePersonService = samplePersonService;
-        addClassNames("master-detail-view");
+	public MasterDetailView(final SamplePersonService samplePersonService) {
+		this.samplePersonService = samplePersonService;
+		addClassNames("master-detail-view");
 
-        // Create UI
-        SplitLayout splitLayout = new SplitLayout();
+		// Create UI
+		final SplitLayout splitLayout = new SplitLayout();
 
-        createGridLayout(splitLayout);
-        createEditorLayout(splitLayout);
+		createGridLayout(splitLayout);
+		createEditorLayout(splitLayout);
 
-        add(splitLayout);
+		add(splitLayout);
 
-        // Configure Grid
-        grid.addColumn("firstName").setAutoWidth(true);
-        grid.addColumn("lastName").setAutoWidth(true);
-        grid.addColumn("email").setAutoWidth(true);
-        grid.addColumn("phone").setAutoWidth(true);
-        grid.addColumn("dateOfBirth").setAutoWidth(true);
-        grid.addColumn("occupation").setAutoWidth(true);
-        grid.addColumn("role").setAutoWidth(true);
-        LitRenderer<SamplePerson> importantRenderer = LitRenderer.<SamplePerson>of(
-                "<vaadin-icon icon='vaadin:${item.icon}' style='width: var(--lumo-icon-size-s); height: var(--lumo-icon-size-s); color: ${item.color};'></vaadin-icon>")
-                .withProperty("icon", important -> important.isImportant() ? "check" : "minus").withProperty("color",
-                        important -> important.isImportant()
-                                ? "var(--lumo-primary-text-color)"
-                                : "var(--lumo-disabled-text-color)");
+		// Configure Grid
+		grid.addColumn("firstName").setAutoWidth(true);
+		grid.addColumn("lastName").setAutoWidth(true);
+		grid.addColumn("email").setAutoWidth(true);
+		grid.addColumn("phone").setAutoWidth(true);
+		grid.addColumn("dateOfBirth").setAutoWidth(true);
+		grid.addColumn("occupation").setAutoWidth(true);
+		grid.addColumn("role").setAutoWidth(true);
+		final LitRenderer<SamplePerson> importantRenderer = LitRenderer.<SamplePerson>of(
+				"<vaadin-icon icon='vaadin:${item.icon}' style='width: var(--lumo-icon-size-s); height: var(--lumo-icon-size-s); color: ${item.color};'></vaadin-icon>")
+				.withProperty("icon", important -> important.isImportant() ? "check" : "minus")
+				.withProperty("color", important -> important.isImportant() ? "var(--lumo-primary-text-color)"
+						: "var(--lumo-disabled-text-color)");
 
-        grid.addColumn(importantRenderer).setHeader("Important").setAutoWidth(true);
+		grid.addColumn(importantRenderer).setHeader("Important").setAutoWidth(true);
 
-        grid.setItems(query -> samplePersonService.list(VaadinSpringDataHelpers.toSpringPageRequest(query)).stream());
-        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
+		grid.setItems(query -> samplePersonService.list(VaadinSpringDataHelpers.toSpringPageRequest(query)).stream());
+		grid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
 
-        // when a row is selected or deselected, populate form
-        grid.asSingleSelect().addValueChangeListener(event -> {
-            if (event.getValue() != null) {
-                UI.getCurrent().navigate(String.format(SAMPLEPERSON_EDIT_ROUTE_TEMPLATE, event.getValue().getId()));
-            } else {
-                clearForm();
-                UI.getCurrent().navigate(MasterDetailView.class);
-            }
-        });
+		// when a row is selected or deselected, populate form
+		grid.asSingleSelect().addValueChangeListener(event -> {
+			if (event.getValue() != null)
+				UI.getCurrent().navigate(String.format(SAMPLEPERSON_EDIT_ROUTE_TEMPLATE, event.getValue().getId()));
+			else {
+				clearForm();
+				UI.getCurrent().navigate(MasterDetailView.class);
+			}
+		});
 
-        // Configure Form
-        binder = new BeanValidationBinder<>(SamplePerson.class);
+		// Configure Form
+		binder = new BeanValidationBinder<>(SamplePerson.class);
 
-        // Bind fields. This is where you'd define e.g. validation rules
+		// Bind fields. This is where you'd define e.g. validation rules
 
-        binder.bindInstanceFields(this);
+		binder.bindInstanceFields(this);
 
-        cancel.addClickListener(e -> {
-            clearForm();
-            refreshGrid();
-        });
+		cancel.addClickListener(e -> {
+			clearForm();
+			refreshGrid();
+		});
 
-        save.addClickListener(e -> {
-            try {
-                if (this.samplePerson == null) {
-                    this.samplePerson = new SamplePerson();
-                }
-                binder.writeBean(this.samplePerson);
-                samplePersonService.save(this.samplePerson);
-                clearForm();
-                refreshGrid();
-                Notification.show("Data updated");
-                UI.getCurrent().navigate(MasterDetailView.class);
-            } catch (ObjectOptimisticLockingFailureException exception) {
-                Notification n = Notification.show(
-                        "Error updating the data. Somebody else has updated the record while you were making changes.");
-                n.setPosition(Position.MIDDLE);
-                n.addThemeVariants(NotificationVariant.LUMO_ERROR);
-            } catch (ValidationException validationException) {
-                Notification.show("Failed to update the data. Check again that all values are valid");
-            }
-        });
-    }
+		save.addClickListener(e -> {
+			try {
+				if (samplePerson == null)
+					samplePerson = new SamplePerson();
+				binder.writeBean(samplePerson);
+				samplePersonService.save(samplePerson);
+				clearForm();
+				refreshGrid();
+				Notification.show("Data updated");
+				UI.getCurrent().navigate(MasterDetailView.class);
+			} catch (final ObjectOptimisticLockingFailureException exception) {
+				final Notification n = Notification.show(
+						"Error updating the data. Somebody else has updated the record while you were making changes.");
+				n.setPosition(Position.MIDDLE);
+				n.addThemeVariants(NotificationVariant.LUMO_ERROR);
+			} catch (final ValidationException validationException) {
+				Notification.show("Failed to update the data. Check again that all values are valid");
+			}
+		});
+	}
 
-    @Override
-    public void beforeEnter(BeforeEnterEvent event) {
-        Optional<Long> samplePersonId = event.getRouteParameters().get(SAMPLEPERSON_ID).map(Long::parseLong);
-        if (samplePersonId.isPresent()) {
-            Optional<SamplePerson> samplePersonFromBackend = samplePersonService.get(samplePersonId.get());
-            if (samplePersonFromBackend.isPresent()) {
-                populateForm(samplePersonFromBackend.get());
-            } else {
-                Notification.show(
-                        String.format("The requested samplePerson was not found, ID = %s", samplePersonId.get()), 3000,
-                        Notification.Position.BOTTOM_START);
-                // when a row is selected but the data is no longer available,
-                // refresh grid
-                refreshGrid();
-                event.forwardTo(MasterDetailView.class);
-            }
-        }
-    }
+	@Override
+	public void beforeEnter(final BeforeEnterEvent event) {
+		final Optional<Long> samplePersonId = event.getRouteParameters().get(SAMPLEPERSON_ID).map(Long::parseLong);
+		if (samplePersonId.isPresent()) {
+			final Optional<SamplePerson> samplePersonFromBackend = samplePersonService.get(samplePersonId.get());
+			if (samplePersonFromBackend.isPresent())
+				populateForm(samplePersonFromBackend.get());
+			else {
+				Notification.show(
+						String.format("The requested samplePerson was not found, ID = %s", samplePersonId.get()), 3000,
+						Notification.Position.BOTTOM_START);
+				// when a row is selected but the data is no longer available,
+				// refresh grid
+				refreshGrid();
+				event.forwardTo(MasterDetailView.class);
+			}
+		}
+	}
 
-    private void createEditorLayout(SplitLayout splitLayout) {
-        Div editorLayoutDiv = new Div();
-        editorLayoutDiv.setClassName("editor-layout");
+	private void clearForm() {
+		populateForm(null);
+	}
 
-        Div editorDiv = new Div();
-        editorDiv.setClassName("editor");
-        editorLayoutDiv.add(editorDiv);
+	private void createButtonLayout(final Div editorLayoutDiv) {
+		final HorizontalLayout buttonLayout = new HorizontalLayout();
+		buttonLayout.setClassName("button-layout");
+		cancel.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+		save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+		buttonLayout.add(save, cancel);
+		editorLayoutDiv.add(buttonLayout);
+	}
 
-        FormLayout formLayout = new FormLayout();
-        firstName = new TextField("First Name");
-        lastName = new TextField("Last Name");
-        email = new TextField("Email");
-        phone = new TextField("Phone");
-        dateOfBirth = new DatePicker("Date Of Birth");
-        occupation = new TextField("Occupation");
-        role = new TextField("Role");
-        important = new Checkbox("Important");
-        formLayout.add(firstName, lastName, email, phone, dateOfBirth, occupation, role, important);
+	private void createEditorLayout(final SplitLayout splitLayout) {
+		final Div editorLayoutDiv = new Div();
+		editorLayoutDiv.setClassName("editor-layout");
 
-        editorDiv.add(formLayout);
-        createButtonLayout(editorLayoutDiv);
+		final Div editorDiv = new Div();
+		editorDiv.setClassName("editor");
+		editorLayoutDiv.add(editorDiv);
 
-        splitLayout.addToSecondary(editorLayoutDiv);
-    }
+		final FormLayout formLayout = new FormLayout();
+		firstName = new TextField("First Name");
+		lastName = new TextField("Last Name");
+		email = new TextField("Email");
+		phone = new TextField("Phone");
+		dateOfBirth = new DatePicker("Date Of Birth");
+		occupation = new TextField("Occupation");
+		role = new TextField("Role");
+		important = new Checkbox("Important");
+		formLayout.add(firstName, lastName, email, phone, dateOfBirth, occupation, role, important);
 
-    private void createButtonLayout(Div editorLayoutDiv) {
-        HorizontalLayout buttonLayout = new HorizontalLayout();
-        buttonLayout.setClassName("button-layout");
-        cancel.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        buttonLayout.add(save, cancel);
-        editorLayoutDiv.add(buttonLayout);
-    }
+		editorDiv.add(formLayout);
+		createButtonLayout(editorLayoutDiv);
 
-    private void createGridLayout(SplitLayout splitLayout) {
-        Div wrapper = new Div();
-        wrapper.setClassName("grid-wrapper");
-        splitLayout.addToPrimary(wrapper);
-        wrapper.add(grid);
-    }
+		splitLayout.addToSecondary(editorLayoutDiv);
+	}
 
-    private void refreshGrid() {
-        grid.select(null);
-        grid.getDataProvider().refreshAll();
-    }
+	private void createGridLayout(final SplitLayout splitLayout) {
+		final Div wrapper = new Div();
+		wrapper.setClassName("grid-wrapper");
+		splitLayout.addToPrimary(wrapper);
+		wrapper.add(grid);
+	}
 
-    private void clearForm() {
-        populateForm(null);
-    }
+	private void populateForm(final SamplePerson value) {
+		samplePerson = value;
+		binder.readBean(samplePerson);
 
-    private void populateForm(SamplePerson value) {
-        this.samplePerson = value;
-        binder.readBean(this.samplePerson);
+	}
 
-    }
+	private void refreshGrid() {
+		grid.select(null);
+		grid.getDataProvider().refreshAll();
+	}
 }
